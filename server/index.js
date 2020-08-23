d3667dff4132bc57c551764ec28cfcb51fc5c88c
evolveris <20345716+evolveris@users.noreply.github.com>
2020-08-23 19:18:56 +0100

    add express (server-side)

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+// const monk = require('monk');
+
+const app = express();
+
+app.enable('trust proxy');
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Test! 😹 🐈'
+  });
+});
+
+
+app.use((error, req, res, next) => {
+  res.status(500);
+  res.json({
+    message: error.message
+  });
+});
+
+app.listen(5000, () => {
+  console.log('Listening on http://localhost:5000');
+});
